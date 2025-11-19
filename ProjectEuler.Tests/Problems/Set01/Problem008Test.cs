884014ad9a31d8b22c3aa69c0df3d0ee011b1c58
Karuppasamy, Uma Arasi <umaarasi@m-s.in>
2025-11-19 11:59:43 +0530

    ADDED: Problem8, Problem8 Test

---
+++ ProjectEuler.Tests/Problems/Set01/Problem008Test.cs
@@ -0,0 +1,30 @@
+﻿using ProjectEuler.Problems;
+
+namespace ProjectEuler.Tests;
+
+[TestClass]
+public class Problem008Test
+{
+	#region Tests
+	[TestMethod]
+	public void Test_Solve_ReturnsCorrectPrime_ForDefaultValue()
+	{
+		//Arrange
+		Problem008 problem = new Problem008();
+		//Act
+		object result = problem.Solve();
+		//Assert
+		Assert.AreEqual((long) 5832, result);
+	}
+	[TestMethod]
+	public void Test_Solve_ReturnsCorrectPrime_ForGivenValue()
+	{
+		//Arrange
+		Problem008 problem = new Problem008();
+		//Act
+		object result = problem.Solve(13);
+		//Assert
+		Assert.AreEqual((long) 23514624000, result);
+	}
+	#endregion
+}
